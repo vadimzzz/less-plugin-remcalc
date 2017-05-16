bc8ce9c3e93f3362bad67bd11eb6ad8b46b6e3c7
Vadim Zozulya <vadim.z.88@gmail.com>
2017-05-16 14:10:34 +0300

    add tests for disabled state

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ const plugin = require(path.join(__dirname, '..', 'dist', 'plugin'));
 const options = { strictMath: true, silent: true, plugins: [plugin] };
 
 tester.runTestSet(options, 'functions/');
+tester.runTestSet(options, 'disabled/');
 
 if (tester.finish) {
     test.finish();
